5a29185b5c1c0178597a81aeec1b9a258533caa0
Elizabeth Neary <20071724@mail.wit.ie>
2024-01-20 09:37:22 +0000

    Higher-order functions

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,14 +1,16 @@
-import { friends } from './01-basics'
-import { colleagues } from './01-basics'
-import {Friend, Colleague } from './myTypes'
+import { friends,colleagues } from './01-basics'
+import {Friend, Colleague, EmailContact } from './myTypes'
 
+//function older(f: Friend) : string 
+// { (Type Leveraging) (:string) removed
 
-function older(f: Friend) : string {
+function older(f: Friend) {
     f.age += 1
     return `${f.name} is now ${f.age}` 
+    
 }
-
-function allOlder(friendsArray: Friend[]): string[] {
+// :string[] removed type leveraging
+function allOlder(friendsArray: Friend[]) {
     return friendsArray.map((friend) => older(friend));
 }
 
@@ -17,14 +19,12 @@ console.log(allOlder(friends));
 // ---------------------------------------------
 
 // Find the colleague with the highest extension number.
-function highestExtension(cs: Colleague[]): Colleague {
+function highestExtension(cs: Colleague[]) {
     const result = cs.sort(
       (c1, c2) => c1.contact.extension - c2.contact.extension
     );
     return result[cs.length - 1];
   }
-  console.log(highestExtension(colleagues.current));
-
 
 
 // Exercise
@@ -49,3 +49,26 @@ function addColleague(currentColleagues: Colleague[], name: string, department: 
 addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
 console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
 
+// -------------------------------------
+
+function sortColleagues(
+    colleagues: Colleague[],
+    sorter: (c1: Colleague, c2: Colleague) => number
+  ): EmailContact[] {
+    const sorted = colleagues.sort(sorter); // Colleague[] inferred
+    const result: EmailContact[] = sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
+    return result 
+  }
+  
+  console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
+  console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
+
+//   -------------------Exercise---------------------------
+
+function findFriends(friendsArray: Friend[], criterion: (friend: Friend) => boolean): string[] {
+    return friendsArray.filter(criterion).map(friend => friend.name);
+}
+
+// Example usage:
+console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
+console.log(findFriends(friends, (friend) => friend.age < 35));
